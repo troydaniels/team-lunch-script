84c724be04f1340ad7bf98eb7760d652e5f426a6
OMG <omg@192168015116.tpgi.com.au>
2016-05-05 14:44:44 +1000

    Small modification to form

--- src/index.php
+++ src/index.php
@@ -1,20 +1,20 @@
 <html>
    <body>
-      <form action="/" method="post">
+      <form action="/" method="post" autocomplete="off">
          <p>Riddle answer: <input type="text" name="answer" /></p>
          <p><input type="submit" /></p>
       </form>
       <br>
       <?php
-      $answerOne = 'one';
+      $answerOne = 'eye';
       $answerTwo = 'two';
       $answerThree = 'three';
 
-      if (strpos($_POST["answer"], $answerOne) !== false){
+      if (stripos($_POST["answer"], $answerOne) !== false){
          echo '<a href="http://www.exampleone.com">http://www.exampleone.com</a>';
-      } elseif (strpos($_POST["answer"], $answerTwo) !== false){
+      } elseif (stripos($_POST["answer"], $answerTwo) !== false){
          echo '<a href="http://www.exampletwo.com">http://www.exampletwo.com</a>';
-      } elseif (strpos($_POST["answer"], $answerThree) !== false){
+      } elseif (stripos($_POST["answer"], $answerThree) !== false){
          echo '<a href="http://www.examplethree.com">http://www.examplethree.com</a>';
       }
       ?>
